62202b3299fc05aa92a0936e47af62e1e477c948
BuildTools <52896585+DecentralisedTech@users.noreply.github.com>
2019-10-21 14:31:30 +1300

    First Commit

---
+++ contracts/token.sol
@@ -0,0 +1,15 @@
+/*
+    PAX Token Reserve (PAXTR)
+    ERC20 Token for the People
+    https://paxco.in/
+    -
+    Developed by James Galbraith, https://Decentralised.Tech/
+*/
+pragma solidity ^0.5.11;
+
+contract PAXTR {
+    constructor() public payable {
+
+
+    }
+}
