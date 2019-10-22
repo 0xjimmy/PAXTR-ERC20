1620b77b2451acbe8b80e378ea3856bf9b339e57
BuildTools <52896585+DecentralisedTech@users.noreply.github.com>
2019-10-22 14:14:06 +1300

    Daily Commit

--- contracts/token.sol
+++ contracts/token.sol
@@ -9,15 +9,6 @@
 pragma solidity ^0.5.11;
 
 library SafeMath {
-    /**
-     * @dev Returns the addition of two unsigned integers, reverting on
-     * overflow.
-     *
-     * Counterpart to Solidity's `+` operator.
-     *
-     * Requirements:
-     * - Addition cannot overflow.
-     */
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 c = a + b;
         require(c >= a, "SafeMath: addition overflow");
@@ -25,31 +16,10 @@ library SafeMath {
         return c;
     }
 
-    /**
-     * @dev Returns the subtraction of two unsigned integers, reverting on
-     * overflow (when the result is negative).
-     *
-     * Counterpart to Solidity's `-` operator.
-     *
-     * Requirements:
-     * - Subtraction cannot overflow.
-     */
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
         return sub(a, b, "SafeMath: subtraction overflow");
     }
 
-    /**
-     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
-     * overflow (when the result is negative).
-     *
-     * Counterpart to Solidity's `-` operator.
-     *
-     * Requirements:
-     * - Subtraction cannot overflow.
-     *
-     * NOTE: This is a feature of the next version of OpenZeppelin Contracts.
-     * @dev Get it via `npm install @openzeppelin/contracts@next`.
-     */
     function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
         require(b <= a, errorMessage);
         uint256 c = a - b;
@@ -57,15 +27,6 @@ library SafeMath {
         return c;
     }
 
-    /**
-     * @dev Returns the multiplication of two unsigned integers, reverting on
-     * overflow.
-     *
-     * Counterpart to Solidity's `*` operator.
-     *
-     * Requirements:
-     * - Multiplication cannot overflow.
-     */
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
         // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
         // benefit is lost if 'b' is also tested.
@@ -80,34 +41,10 @@ library SafeMath {
         return c;
     }
 
-    /**
-     * @dev Returns the integer division of two unsigned integers. Reverts on
-     * division by zero. The result is rounded towards zero.
-     *
-     * Counterpart to Solidity's `/` operator. Note: this function uses a
-     * `revert` opcode (which leaves remaining gas untouched) while Solidity
-     * uses an invalid opcode to revert (consuming all remaining gas).
-     *
-     * Requirements:
-     * - The divisor cannot be zero.
-     */
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
         return div(a, b, "SafeMath: division by zero");
     }
 
-    /**
-     * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
-     * division by zero. The result is rounded towards zero.
-     *
-     * Counterpart to Solidity's `/` operator. Note: this function uses a
-     * `revert` opcode (which leaves remaining gas untouched) while Solidity
-     * uses an invalid opcode to revert (consuming all remaining gas).
-     *
-     * Requirements:
-     * - The divisor cannot be zero.
-     * NOTE: This is a feature of the next version of OpenZeppelin Contracts.
-     * @dev Get it via `npm install @openzeppelin/contracts@next`.
-     */
     function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
         // Solidity only automatically asserts when dividing by 0
         require(b > 0, errorMessage);
@@ -117,44 +54,101 @@ library SafeMath {
         return c;
     }
 
-    /**
-     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
-     * Reverts when dividing by zero.
-     *
-     * Counterpart to Solidity's `%` operator. This function uses a `revert`
-     * opcode (which leaves remaining gas untouched) while Solidity uses an
-     * invalid opcode to revert (consuming all remaining gas).
-     *
-     * Requirements:
-     * - The divisor cannot be zero.
-     */
     function mod(uint256 a, uint256 b) internal pure returns (uint256) {
         return mod(a, b, "SafeMath: modulo by zero");
     }
 
-    /**
-     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
-     * Reverts with custom message when dividing by zero.
-     *
-     * Counterpart to Solidity's `%` operator. This function uses a `revert`
-     * opcode (which leaves remaining gas untouched) while Solidity uses an
-     * invalid opcode to revert (consuming all remaining gas).
-     *
-     * Requirements:
-     * - The divisor cannot be zero.
-     *
-     * NOTE: This is a feature of the next version of OpenZeppelin Contracts.
-     * @dev Get it via `npm install @openzeppelin/contracts@next`.
-     */
     function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
         require(b != 0, errorMessage);
         return a % b;
     }
 }
 
-contract PAXTRS {
+contract Owned {
+    address public owner;
+    address public newOwner;
+    modifier onlyOwner {
+        require(msg.sender == owner, 'Address not contract owner');
+        _;
+    }
+    function transferOwnership(address _newOwner) public onlyOwner {
+        newOwner = _newOwner;
+    }
+    function acceptOwnership() public {
+        require(msg.sender == newOwner, 'Address not contract owner');
+        owner = newOwner;
+    }
+}
+
+contract PAXTR is Owned {
     using SafeMath for uint256;
+
     constructor() public payable {
 
     }
+
+    // Events
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    // Main Variables
+    address public minterAddress;
+    address public worldTresuryAddress;
+
+    uint256 public createdAccounts = 0;
+    uint256 public activeAccounts = 0;
+    uint256 public maximumSupply = 0;
+    uint256 public circulatingBaseSupply = 0;
+
+    uint256 public endOfMonth;
+
+    // Demurrage base
+    uint256 public demurrageBaseMultiplier = 1000000000000000000;
+
+    // Balance Mapping
+    mapping(address => uint256) public baseBalance;
+
+    struct Treasure {
+        uint256 totalClaimed;
+        mapping(uint256 => uint256) claimedInMonth;
+    }
+    mapping(address => Treasure) public treasure;
+    mapping(address => bool) public hasTreasure;
+
+    // Minting
+    function issueTreasure(address account) public {
+        require(msg.sender == minterAddress, 'Only the minterAddress may call this function');
+        require(hasTreasure[account] == false, 'Account has already been issued their Lifetime Treasure');
+        hasTreasure[account] = true;
+        // To to
+    }
+
+    // ERC20 Standard Functions
+
+    function totalSupply() public view returns (uint256) {
+        return (circulatingBaseSupply.mul(demurrageBaseMultiplier)).div(1000000000000000000);
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return (baseBalance[account].mul(demurrageBaseMultiplier)).div(1000000000000000000);
+    }
+
+    function transfer(address recipient, uint256 amount) external returns (bool) {
+        require(balanceOf(msg.sender) >= amount, 'Sender does not have enough balance');
+        uint256 baseAmount = (amount.mul(1000000000000000000)).div(demurrageBaseMultiplier);
+        baseBalance[msg.sender] = sub(baseBalance[msg.sender], baseAmount);
+        baseBalance[recipient] = add(baseBalance[recipient], baseAmount);
+        emit Transfer(msg.sender, recipient, amount);
+        // Credit Treasure is eligible
+        if (hasTreasure[msg.sender] == true && treasure[msg.sender].totalClaimed < && treasure[msg.sender].claimedInMonth[currentMonth]) {
+
+        }
+    }
+
+    // function allowance(address owner, address spender) external view returns (uint256);
+
+    // function approve(address spender, uint256 amount) external returns (bool);
+
+    // function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
 }
